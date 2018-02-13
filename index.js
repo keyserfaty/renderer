2fd0d9298bbed2c2a84a3de3e2bf990170d8cae2
keyserfaty <keserfaty@gmail.com>
2018-02-13 19:58:11 -0300

    made a few fixes to functions

---
+++ index.js
@@ -0,0 +1,9 @@
+var h = require('./lib/h')
+var mount = require('./lib/mount')
+var update = require('./lib/update')
+
+module.exports = {
+  h: h,
+  mount: mount,
+  update: update
+}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-var h = require('./h')
-var mount = require('./mount')
-var update = require('./update')
-
-module.exports = {
-  h: h,
-  mount: mount,
-  update: update
-}
